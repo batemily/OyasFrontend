97ff8d1a98637045d587ec2706dff7392c9461e2
batemily <batasemilie@gmail.com>
2021-10-24 19:48:48 +0200

    second commit

---
+++ src/environments/mqttEnvironment.ts
@@ -0,0 +1,15 @@
+// MQTT environment
+export const environment = {
+  production: true,
+  //   hmr: false,
+  //   http: {
+  //     apiUrl: 'https://sensorultra.data.thethingsnetwork.org/api/v2/query',
+  //   },
+  mqtt: {
+    server: 'asia-se.thethings.network',
+    protocol: 'wss',
+    port: 8883,
+    username: 'sensorultra',
+    password: 'key ttn-account-v2.g7M2c9s1lvj-ySg3cAc-CmAJw0thqRSFxrjFd3k7AVY',
+  },
+};
